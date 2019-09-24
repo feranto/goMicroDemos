773d98bf4815618c5186251b1b429bccd63c8fe5
Fernando Antonio Mejia Chavez <feranto@AYANBASB.fareast.corp.microsoft.com>
2019-09-24 17:48:10 -0300

    basic number operations

---
+++ src/Basic Exercises/2. Numbers and Assignments/code2.go
@@ -0,0 +1,21 @@
+package main 
+
+import (
+	"fmt"
+)
+
+func main(){
+	var x float32
+	var y float32
+	
+	x = 1.0
+	y = 2.0
+
+	fmt.Printf("x=%v, type of %T\n", x, x)
+	fmt.Printf("y=%v, type of %T\n", y, y)
+
+	var mean float32
+	mean = (x + y)/2
+	fmt.Printf("result: %v, type of %T\n ", mean,mean)
+
+}
